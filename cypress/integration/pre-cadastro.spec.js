33da5669c4779c0ee0e46894bbe743de67c46296
Rodrigo Marcal <rodrigofmarcal@yahoo.com.br>
2022-08-15 19:47:13 -0300

    meu primeiro commit

--- cypress/integration/pre-cadastro.spec.js
+++ cypress/integration/pre-cadastro.spec.js
@@ -1,4 +1,5 @@
 ///<reference types="cypress" />
+var faker = require('faker');
 
 describe('Funcionalidade de pré cadastro', () => {
 
@@ -6,17 +7,17 @@ describe('Funcionalidade de pré cadastro', () => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
     });
     
-
 it('Deve completar o pré cadastro com sucesso', () => {
-    cy.get('#reg_email').type('rfmteste99@teste.com')
+    cy.get('#reg_email').type(faker.internet.email())
     cy.get('#reg_password').type('teste@teste$')
     cy.get(':nth-child(4) > .button').click()
 
     cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
-    cy.get('#account_first_name').type(Rodrigo)
-    cy.get('#account_last_name').type(Marçal)
+    cy.get('#account_first_name').type(faker.name.firstName())
+    cy.get('#account_last_name').type(faker.name.lastName())
     cy.get('.woocommerce-Button').click()
-    
+
+    cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
 
 
 });
