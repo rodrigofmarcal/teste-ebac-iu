929e128b9acacbca1bc015c99eebb2ab47d08b07
Rodrigo Marcal <rodrigofmarcal@yahoo.com.br>
2022-08-16 21:46:52 -0300

    alteracoes no teste automatizado

---
+++ cypress/integration/produtos.spec.js
@@ -0,0 +1,33 @@
+/// <reference types="cypress" />
+
+describe('Funcionalidade Página de produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/page/3/')
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('[class="product-block grid"]')
+            //.first()
+            //.last()
+            //.eq(3)
+            .contains('Cassius Sparring Tank')
+            .click()
+    });
+
+    it('Deve adicionar um produto ao carrinho', () => {
+        var quantidade = 3
+
+        cy.get('[class="product-block grid"]')
+            .contains('Cassius Sparring Tank').click()
+        cy.get('.button-variable-item-XL').click()
+        cy.get(':nth-child(2) > .value > .variable-items-wrapper > .variable-item').click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+        cy.get('.woocommerce-message').should('contain', quantidade + ' × “Cassius Sparring Tank” foram adicionados no seu carrinho')
+
+    });
+
+});
