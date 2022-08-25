bfa64c8dc5b26bd45d10e922c0b7d7202817fe5f
Rodrigo Marcal <rodrigofmarcal@yahoo.com.br>
2022-08-25 20:52:10 -0300

    usando massa de dados

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,39 +1,60 @@
 ///<reference types="cypress" />
+const perfil = require('..//fixtures/perfil.json')
 
-context('funcionalidade login',()=>{
+context('funcionalidade login', () => {
 
     beforeEach(() => {
-        cy.visit('minha-conta')    
+        cy.visit('minha-conta')
     });
-    
+
     afterEach(() => {
         cy.screenshot()
     });
 
 
-    it('Deve fazer login com sucesso',()=>{
-cy.get('#username').type('aluno_ebac@teste.com')
-cy.get('#password').type('teste@teste.com')
-cy.get('.woocommerce-form > .button').click()
+    it('Deve fazer login com sucesso', () => {
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('teste@teste.com')
+        cy.get('.woocommerce-form > .button').click()
 
-cy.get('.page-title').should('contain','Minha conta')
-cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain','Olá,')
+        cy.get('.page-title').should('contain', 'Minha conta')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá,')
     })
 
-    it('Deve exibir uma mensagem de erro ao inserir usuário inválido ',()=>{
-    cy.get('#username').type('ebacc@teste.com')
-    cy.get('#password').type('teste@teste')
-    cy.get('.woocommerce-form > .button').click()
+    it('Deve fazer login com sucesso - Usando arquivo de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
 
-    cy.get('.woocommerce-error').should('contain','Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
+        cy.get('.page-title').should('contain', 'Minha conta')
+
+        it('Deve fazer login com sucesso - Usando fixture', () => {
+            cy.fixture('perfil').then(dados => {
+                cy.get('#username').type(dados.usuario)
+                cy.get('#password').type(dados.senha, {log: false})
+                cy.get('.woocommerce-form > .button').click()
+
+                cy.get('.page-title').should('contain', 'Minha conta') 
+            })
+
+        });
+
+    });
+
+    it('Deve exibir uma mensagem de erro ao inserir usuário inválido ', () => {
+        cy.get('#username').type('ebacc@teste.com')
+        cy.get('#password').type('teste@teste')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-error').should('contain', 'Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
     })
 
-    it('Deve exibir uma mensagem de erro ao inserir senha inválida ',()=>{
+    it('Deve exibir uma mensagem de erro ao inserir senha inválida ', () => {
         cy.get('#username').type('ebac@teste.com')
         cy.get('#password').type('teste@teste')
         cy.get('.woocommerce-form > .button').click()
 
-        cy.get('.woocommerce-error > li').should('contain','Erro: A senha fornecida para o e-mail')
-        
-    })  
+        cy.get('.woocommerce-error > li').should('contain', 'Erro: A senha fornecida para o e-mail')
+
+    })
 })
