6d968cc4cb22f2d0ad8ca21703a9239c510a4d4a
Rodrigo Marcal <rodrigofmarcal@yahoo.com.br>
2022-08-27 11:38:43 -0300

    usando massa de dados

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -11,7 +11,6 @@ context('funcionalidade login', () => {
         cy.screenshot()
     });
 
-
     it('Deve fazer login com sucesso', () => {
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
@@ -27,6 +26,7 @@ context('funcionalidade login', () => {
         cy.get('.woocommerce-form > .button').click()
 
         cy.get('.page-title').should('contain', 'Minha conta')
+    });
 
         it('Deve fazer login com sucesso - Usando fixture', () => {
             cy.fixture('perfil').then(dados => {
@@ -39,8 +39,6 @@ context('funcionalidade login', () => {
 
         });
 
-    });
-
     it('Deve exibir uma mensagem de erro ao inserir usuário inválido ', () => {
         cy.get('#username').type('ebacc@teste.com')
         cy.get('#password').type('teste@teste')
@@ -48,13 +46,14 @@ context('funcionalidade login', () => {
 
         cy.get('.woocommerce-error').should('contain', 'Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
     })
+ 
 
     it('Deve exibir uma mensagem de erro ao inserir senha inválida ', () => {
-        cy.get('#username').type('ebac@teste.com')
+        cy.get('#username') .type('ebac@teste.com')
         cy.get('#password').type('teste@teste')
         cy.get('.woocommerce-form > .button').click()
 
         cy.get('.woocommerce-error > li').should('contain', 'Erro: A senha fornecida para o e-mail')
 
-    })
+    }) 
 })
