13730ada1b275f4ffd7521189ee23280b4e4f83b
Rodrigo Marcal <rodrigofmarcal@yahoo.com.br>
2022-09-01 19:59:37 -0300

    editar e cadastrar endereco

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,16 +1,17 @@
 /// <reference types="cypress" />
+import EnderecoPage from '../support/page-objects/endereco.page'
 
-describe('Funcionalidade Endereços - Faturamento e entregas', () => {
-   beforeEach(() => {
-    cy.visit('minha-conta')
-    cy.fixture('perfil').then(dados =>{
-        cy.login(dados.usuario, dados.senha)
-    })
-    
-   });
-   
-    it('Deve fazer cadastro de faturamento com sucesso', () => {
-        
-        //cadastro de endereço
+describe('Funcionalidade Endereços - Faturamento e entrega', () => {
+    beforeEach(() => {
+        cy.visit('minha-conta')
+        cy.fixture('perfil').then(dados => {
+            cy.login(dados.usuario, dados.senha)
+        })
+
+    });
+
+    it.only('Deve fazer cadastro de faturamento com sucesso', () => {
+        EnderecoPage.editarEnderecoFaturamento('Rodrigo', 'Marcal', 'UBER', 'Brasil', 'rua Azul', '10', 'BH', 'Minas Gerais', '30 000 000', '3101010101', 'foraBolsona@imbecil.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
 });
