07ee1a185e5e5efbb0e8bc4cb268e6fc10760137
Rodrigo Marcal <rodrigofmarcal@yahoo.com.br>
2022-09-06 18:56:18 -0300

    massa de dados em lista

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -19,16 +19,16 @@ describe('Funcionalidade Endereços - Faturamento e entrega', () => {
     it('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
         EnderecoPage.editarEnderecoFaturamento
             (dadosEndereco[1].nome,
-            dadosEndereco[1].sobrenome,
-            dadosEndereco[1].empresa,
-            dadosEndereco[1].pais,
-            dadosEndereco[1].endereco,
-            dadosEndereco[1].numero,
-            dadosEndereco[1].cidade,
-            dadosEndereco[1].estado,
-            dadosEndereco[1].cep,
-            dadosEndereco[1].telefone,
-            dadosEndereco[1].email)
+                dadosEndereco[1].sobrenome,
+                dadosEndereco[1].empresa,
+                dadosEndereco[1].pais,
+                dadosEndereco[1].endereco,
+                dadosEndereco[1].numero,
+                dadosEndereco[1].cidade,
+                dadosEndereco[1].estado,
+                dadosEndereco[1].cep,
+                dadosEndereco[1].telefone,
+                dadosEndereco[1].email)
         cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
 
     });
